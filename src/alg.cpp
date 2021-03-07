3d1fe15b3caa381c7802f7c9ccfddc3fa43ca7b3
DivineSunderer <78905175+DivineSunderer@users.noreply.github.com>
2021-03-07 16:37:27 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,11 +1,11 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    while ((b ! 0) && (a ! 0)) {
-        if (a > b)
-            a = a - b;
-        else
-            b = b - a;
-    }
-    return (a+b);
+while ((b ! 0) && (a ! 0)) {
+if (a > b)
+a = a - b;
+else
+b = b - a;
+}
+return (a+b);
 }
